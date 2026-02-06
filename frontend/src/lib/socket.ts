1733b6c9b556457329d1ef1649b95a631a4d1980
Jeremy López <jalperez@banreservas.com>
2026-02-06 01:51:24 -0400

    add VITE_SOCKET_URL

--- frontend/src/lib/socket.ts
+++ frontend/src/lib/socket.ts
@@ -1,6 +1,8 @@
 import { io } from 'socket.io-client';
 
-export const socket = io('/', {
+const URL = import.meta.env.VITE_SOCKET_URL || '/';
+
+export const socket = io(URL, {
   autoConnect: true,
   reconnection: true,
   path: '/socket.io',
